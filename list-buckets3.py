0bc3f4450ec597e977a6da279bb40d7381db8070
Ubuntu <ubuntu@ip-172-31-87-100.ec2.internal>
2023-08-10 16:44:38 +0000

    Codigos python buckets

---
+++ list-buckets3.py
@@ -0,0 +1,13 @@
+import boto3
+
+# Retrieve the list of existing buckets
+s3 = boto3.client('s3', 'us-east-1')
+response = s3.list_buckets()
+
+# Output the bucket names
+for bucket in response['Buckets']:
+	bucket_name = bucket['Name']
+	object_list = s3.list_objects_v2(Bucket = bucket_name)
+	print(f'Files in bucket {bucket_name}:')
+	for obj in object_list.get('Contents', []):
+		print('	', obj['Key'])
